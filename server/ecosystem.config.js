2bed40a7eeaf955c37c78e8b1cef435c199775b2
Greg Barry <gregory.paul.barry@gmail.com>
2021-05-28 17:20:25 -0500

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-    apps : [{
-        name: 'tictactoe-server',
-        script: './server.js',
-        watch: false,
-        exec_mode: 'fork',
-        instances: 1,
-        instance_var: 'INSTANCE_ID',
-        env: {
-            'NODE_ENV': 'production',
-            'ALLOW_CONFIG_MUTATIONS': 'Y'
-        }
-    }]
-};
+export const apps = [{
+    name: 'tictactoe-server',
+    script: './server.js',
+    watch: false,
+    exec_mode: 'fork',
+    instances: 1,
+    instance_var: 'INSTANCE_ID',
+    env: {
+        'NODE_ENV': 'production',
+        'ALLOW_CONFIG_MUTATIONS': 'Y'
+    }
+}];
